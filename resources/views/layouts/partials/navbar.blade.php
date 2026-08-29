2dbed808d0641ed5d77b80faf59afc821d280494
MCBANKSKE <mcbankske@gmail.com>
2026-08-29 16:47:13 +0300

    ..

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,8 +1,12 @@
 <nav class="navbar-nav" id="navbar-nav">
+        <a href="{{ route('catalogue.index') }}" class="nav-link">Browse Menu</a>
         <a href="#about" class="nav-link">About</a>
         <a href="#services" class="nav-link">Services</a>
-        <a href="#who-we-serve" class="nav-link">Who We Serve</a>
         <a href="#coverage" class="nav-link">Coverage</a>
         <a href="#feedback" class="nav-link">Feedback</a>
-        <a href="https://wa.me/254737953292?text=Hi%20PMB%2C%20I%27d%20like%20to%20book%20a%20consultation" class="btn btn-gold btn-nav" target="_blank" rel="noopener">Book via WhatsApp</a>
+        @auth
+            <a href="{{ route('customer.dashboard') }}" class="btn btn-gold btn-nav">My Account</a>
+        @else
+            <a href="{{ route('login') }}" class="btn btn-gold btn-nav">Sign In</a>
+        @endauth
       </nav>
